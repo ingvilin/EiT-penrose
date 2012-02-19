588b98c125e01fe84a13df137536262b6de4bebd
Torstein Thode Kristoffersen <torstein.t.k@gmail.com>
2012-02-19 17:17:53 +0100

    prosjektstart

---
+++ src/no/penrose/prosjekt/Om.java
@@ -0,0 +1,12 @@
+package no.penrose.prosjekt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Om extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
---
+++ src/no/penrose/prosjekt/Silisiumspillet.java
@@ -0,0 +1,50 @@
+package no.penrose.prosjekt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Silisiumspillet extends Activity implements OnClickListener {
+	private static Button newGameButton;
+	private static Button aboutButton;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //final Intent i = new Intent(this, Om.class);
+        newGameButton = (Button) findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(this);
+        aboutButton = (Button) findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        /*aboutButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (v.getId() == R.id.about_button) {
+					startActivity(i);
+				}
+			}
+		});*/
+    }
+
+	public void onClick(View v) {
+    	switch(v.getId()) {
+    	case R.id.new_game_button:
+    		toast("Under utvikling!");
+    		break;
+    	case R.id.about_button:
+    		Intent i = new Intent(this, Om.class);
+    		startActivity(i);
+    		break;
+    	}	
+    }
+	
+	protected void toast(String string) {
+		Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
+	}
+}
