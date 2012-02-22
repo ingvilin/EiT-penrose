c6b6d17e9e4401e4e909c353309d3c0c6a44ca88
ingvilin <ingvildindr@gmail.com>
2012-02-22 16:52:30 +0100

    changed from edittext to textview

--- gen/no/penrose/prosjekt/R.java
+++ gen/no/penrose/prosjekt/R.java
@@ -19,31 +19,28 @@ public final class R {
         public static final int steinbrudd=0x7f020005;
     }
     public static final class id {
-        public static final int about_button=0x7f05000d;
+        public static final int about_button=0x7f05000a;
         public static final int avslutt=0x7f050008;
-        public static final int editText1=0x7f05000b;
+        public static final int editText1=0x7f05000c;
         public static final int fabrikk_image1=0x7f050001;
         public static final int fabrikk_image2=0x7f050002;
         public static final int fabrikk_image3=0x7f050003;
         public static final int fabrikk_root=0x7f050000;
-        public static final int imageView1=0x7f05000a;
-        public static final int kjope_stein=0x7f050010;
-        public static final int new_game_button=0x7f05000c;
-        public static final int relativeLayout1=0x7f050009;
+        public static final int kjope_stein=0x7f05000e;
+        public static final int new_game_button=0x7f050009;
         public static final int start_produksjon_elektro=0x7f050004;
         public static final int start_produksjon_legering=0x7f050005;
         public static final int start_produksjon_solcelle=0x7f050006;
-        public static final int steinbrudd_image1=0x7f05000f;
-        public static final int steinbrudd_root=0x7f05000e;
-        public static final int til_fabrikken=0x7f050011;
+        public static final int steinbrudd_image1=0x7f05000d;
+        public static final int steinbrudd_root=0x7f05000b;
+        public static final int til_fabrikken=0x7f05000f;
         public static final int til_steinbruddet=0x7f050007;
     }
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int fabrikk=0x7f030001;
-        public static final int kvarts=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int steinbrudd=0x7f030004;
+        public static final int main=0x7f030002;
+        public static final int steinbrudd=0x7f030003;
     }
     public static final class string {
         public static final int about_button=0x7f040003;
