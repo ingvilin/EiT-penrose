28d5cf4fc3205c3746b605697ce32d050da1902b
ingvilin <ingvildindr@gmail.com>
2012-02-29 10:15:33 +0100

    husker ikke helt hva som blir commitet her

--- gen/no/penrose/prosjekt/R.java
+++ gen/no/penrose/prosjekt/R.java
@@ -23,25 +23,26 @@ public final class R {
         public static final int steinbrudd=0x7f020009;
     }
     public static final class id {
-        public static final int about_button=0x7f05000f;
+        public static final int about_button=0x7f050010;
         public static final int avslutt=0x7f050008;
-        public static final int editText1=0x7f050011;
+        public static final int editText1=0x7f050012;
         public static final int fabrikk_image1=0x7f050001;
         public static final int fabrikk_image2=0x7f050002;
         public static final int fabrikk_image3=0x7f050003;
         public static final int fabrikk_root=0x7f050000;
-        public static final int imageView1=0x7f05000c;
-        public static final int imageView2=0x7f05000d;
+        public static final int imageView1=0x7f05000d;
+        public static final int imageView2=0x7f05000e;
         public static final int imageView3=0x7f05000b;
-        public static final int new_game_button=0x7f05000e;
+        public static final int kvartsText=0x7f05000c;
+        public static final int new_game_button=0x7f05000f;
         public static final int pengerText=0x7f050009;
+        public static final int progressbar=0x7f050013;
         public static final int relativeLayout1=0x7f05000a;
         public static final int start_produksjon_elektro=0x7f050004;
         public static final int start_produksjon_legering=0x7f050005;
         public static final int start_produksjon_solcelle=0x7f050006;
-        public static final int steinbrudd_image1=0x7f050012;
-        public static final int steinbrudd_root=0x7f050010;
-        public static final int til_fabrikken=0x7f050013;
+        public static final int steinbrudd_image1=0x7f050014;
+        public static final int steinbrudd_root=0x7f050011;
         public static final int til_steinbruddet=0x7f050007;
     }
     public static final class layout {
--- src/no/penrose/prosjekt/Steinbrudd.java
+++ src/no/penrose/prosjekt/Steinbrudd.java
@@ -1,82 +1,72 @@
 package no.penrose.prosjekt;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Steinbrudd extends Activity {
-	private Button tilbake;
-	private int kvartLimitAmount = 200;
-	private int kvartsAmount = 0;
-	private Runnable updateKvartsDigging;
-	private TextView kvartsAmountView;
+	private int maksKvartsGrense = 200;
+	private int antallKvarts = 0;
+	private TextView antallKvartsView;
+	private String kvartsTittel ="Kvarts: ";
+	private ProgressBar progressBar;
+	private Handler handler;
+	public static final String KVARTS ="Kvarts";
 
 
-
-	public Steinbrudd(){
-		this.updateKvartsDigging = new Runnable() {
-
-			public void run() {
-				// TODO Auto-generated method stub
-				kvartsAmount = kvartLimitAmount;
-			}
-		};
-	}
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.steinbrudd);
 
-		tilbake = (Button) findViewById(R.id.til_fabrikken);
-//		tilbake.setOnClickListener(this);
-	     
-	    kvartsAmountView = (TextView) findViewById(R.id.editText1);
-	    kvartsAmountView.setText("Kvarts: " + Integer.toString(kvartsAmount) + "/" + kvartLimitAmount);
-	
+		progressBar = (ProgressBar) findViewById(R.id.progressbar);
+		this.handler = new Handler();
+		//		Runnable updateKvartsDigging = new Runnable() {
+		//			
+		//			public void run() {
+		//				// TODO Auto-generated method stub
+		//				antallKvarts = maksKvartsGrense;
+		//			}
+		//		};
+
+		antallKvartsView = (TextView) findViewById(R.id.editText1);
+		antallKvartsView.setText(kvartsTittel + Integer.toString(antallKvarts) + "/" + maksKvartsGrense);
+
+		SharedPreferences settingsKvarts = getSharedPreferences(KVARTS, 0);
+		antallKvarts = settingsKvarts.getInt(KVARTS, 0);
+		antallKvartsView.setText(kvartsTittel + Integer.toString(antallKvarts) + "/" + maksKvartsGrense);
+
 	}
 
-//	public void onClick(View v) {
-//		switch(v.getId()) {
-//		case R.id.til_fabrikken:
-//			//sende tilbake data med putExtra
-//			finish();
-//			break;
-//		}
-//	}
-	
+	//	public void onClick(View v) {
+	//		switch(v.getId()) {
+	//		case R.id.til_fabrikken:
+	//			//sende tilbake data med putExtra
+	//			finish();
+	//			break;
+	//		}
+	//	}
+
 	/** In the steinbrudd.xml file onClick is set to trigger this method. It will start the digging process and count down till it ends and then update the kvarts amount*/
+		
 	public void diggKvarts(final View view){
 		view.setClickable(false);
-		CountDownTimer countDownTimer= new CountDownTimer(30000, 1000) {
 
-		     public void onTick(long millisUntilFinished) {
-		         kvartsAmountView.setText("seconds remaining: " + millisUntilFinished / 1000);
-		     }
-
-		     public void onFinish() {
-		    	 kvartsAmount = kvartLimitAmount;
-		         kvartsAmountView.setText("Kvarts: " + Integer.toString(kvartsAmount) + "/" + kvartLimitAmount);
-		         view.setClickable(true);
-		     }
-		  }.start();
-		  
 	}
-	
 	public void updateKvartsScore(){
-		kvartsAmountView.setText("Kvarts: " + Integer.toString(kvartsAmount) + "/" + kvartLimitAmount);
+		antallKvartsView.setText("Kvarts: " + Integer.toString(antallKvarts) + "/" + maksKvartsGrense);
 	}
-	
+
 	public int getKvartsAmount() {
-		return kvartsAmount;
+		return antallKvarts;
 	}
-	
+
 	public void setKvartsAmount(int kvartsAmount) {
-		this.kvartsAmount = kvartsAmount;
+		this.antallKvarts = kvartsAmount;
 	}
 }
