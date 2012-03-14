806ea1d9404a5c4818b69b2667c7edf8696b567d
Torstein Thode Kristoffersen <torstein.t.k@gmail.com>
2012-03-14 18:48:14 +0100

    Kontor opprettet

---
+++ src/no/penrose/prosjekt/Kontor.java
@@ -0,0 +1,19 @@
+package no.penrose.prosjekt;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+
+public class Kontor extends Activity implements OnClickListener {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	public void onClick(DialogInterface dialog, int which) {
+		
+	}
+	
+}
